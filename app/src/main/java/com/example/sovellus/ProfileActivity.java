ddacba9691aeecf8a97cb2682033275d3d87ad9b
eljas <eljash@metropolia.fi>
2020-12-08 12:09:16 +0200

    ProfileActivity.java -Nimen ja syntymäpäivän vaihto nappien toiminta -Jos Main Activitystä ei tule arvoja, haetaan viimeksi tallennetut aika arvot
    strings.xml
    -suomennoksia ja pari uutta arvoa

--- app/src/main/java/com/example/sovellus/ProfileActivity.java
+++ app/src/main/java/com/example/sovellus/ProfileActivity.java
@@ -1,16 +1,26 @@
 package com.example.sovellus;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Calendar;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -20,6 +30,7 @@ public class ProfileActivity extends AppCompatActivity {
     private static final String LOGTAG = "ProfileActivity.java";
 
     SuperMetodit SM = new SuperMetodit();
+    Mega mega = new Mega(this);
 
     private String weight;
     private String sport;
@@ -33,6 +44,12 @@ public class ProfileActivity extends AppCompatActivity {
     private TextView weightValue;
     private TextView sportValue;
     private TextView screenValue;
+
+    private EditText input;
+    private Button changeNameButton;
+    private Button changeBirthButton;
+
+    private DatePickerDialog.OnDateSetListener newBirthDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,11 +79,17 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        changeNameButton = findViewById(R.id.changeProfileNameButton);
+        changeBirthButton = findViewById(R.id.changeBirthDateButton);
+
+        createChangeButtons();
+
         Intent intent = getIntent();
-        this.weight = String.valueOf(intent.getDoubleExtra("paino",0));
-        this.screen = Integer.toString(intent.getIntExtra("ruutu",0));
-        this.sport = Integer.toString(intent.getIntExtra("urheilu",0));
+        this.weight = intent.getDoubleExtra("paino",0)+" kg";
+        this.screen = SM.convertSeconds(intent.getIntExtra("ruutu",mega.todayObject().screenSec()));
+        this.sport = SM.convertSeconds(intent.getIntExtra("urheilu",mega.todayObject().sportSec()));
     }
+
 
     @Override
     public void finish() {
@@ -119,4 +142,80 @@ public class ProfileActivity extends AppCompatActivity {
         sportValue.setText(sport);
         screenValue.setText(screen);
     }
+
+    private void createChangeButtons(){
+
+        input = new EditText(this);
+
+        //Luodaan dialogi nimen vaihdolle
+        //Mahdollistaa nappia painaessa ponnahdusikkunan esiin tulon joka kysyy uutta nimeä
+        AlertDialog.Builder nameBuilder = new AlertDialog.Builder(this);
+
+        //Asetetaan otsikko, ikoni ja viesti dialogiin
+        nameBuilder.setTitle(R.string.new_user_name);
+        nameBuilder.setIcon(R.drawable.retrad);
+        nameBuilder.setMessage(R.string.name);
+
+        nameBuilder.setView(input);
+
+        //Lisätään "syötä" napin toiminnallisuus sekä napin teksti nimen vaihdolle
+        nameBuilder.setPositiveButton(R.string.submit, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String text = input.getText().toString();
+                userProfile.setName(text);
+                profile.saveProfile(userProfile);
+                updateTextValues();
+                Toast.makeText(getApplicationContext(),text, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //Lisätään "peruuta" napin toiminnallisuus sekä napin teksti nimen vaihdolle
+        nameBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog changeName = nameBuilder.create();
+
+        //Lisätään aktiviteetin nappiin toiminnallisuus joka avaa nimenvaihto dialogin
+        changeNameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeName.show();
+            }
+        });
+
+        //Lisätään syntymäpäivän vaihtoon toiminnallisuus joka avaa
+        //kalenteri ponnahdusikkunan
+        changeBirthButton.setOnClickListener(v -> {
+            Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog dialog = new DatePickerDialog(
+                    getApplicationContext(),
+                    android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                    newBirthDay,
+                    year,month,day);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.show();
+        });
+
+        //Luetaan kalenteri ikkunaan syötetyt arvot profiiliin
+        newBirthDay = (datePicker, year, month, day) -> {
+            month++; // Koska tammikuu = 0
+
+            userProfile.setYear(year);
+            userProfile.setMonth(month);
+            userProfile.setDay(day);
+            profile.saveProfile(userProfile);
+            updateTextValues();
+        };
+
+    }
+
 }
